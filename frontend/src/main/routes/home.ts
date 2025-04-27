f9aaf806f4b6526705c053bbcf22408db623e15d
Obi Davis <obgdavis@gmail.com>
2025-04-27 22:34:10 +0100

    removed example route from home page

--- frontend/src/main/routes/home.ts
+++ frontend/src/main/routes/home.ts
@@ -1,16 +1,7 @@
-import axios from 'axios';
 import { Application } from 'express';
 
 export default function (app: Application): void {
   app.get('/', async (req, res) => {
-    try {
-      // An example of connecting to the backend (a starting point)
-      const response = await axios.get('http://localhost:4000/get-example-case');
-      console.log(response.data);
-      res.render('home', { 'example': response.data });
-    } catch (error) {
-      console.error('Error making request:', error);
-      res.render('home', {});
-    }
+    res.render('home');
   });
 }
